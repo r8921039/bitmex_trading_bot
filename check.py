60168f22406a5ac5a8228eb60d1ffa37c0e1b024
r8921039 <r8921039@hotmail.com>
2023-09-04 02:35:44 +0000

    add qty which can be multiplyer of 100 or 200. the exercise is to reduce from 200 to 100. WARNINGgit add check.pygit add check.pygit add check.py there is a bug for bootstrapping if there is no current orders. For bootstraping, 1) start check.py with NOFIX, i.e. ./check.py without -f 2) Manually entry first/last buy/sell x00/1000 orders as the TARGET ORDER

--- check.py
+++ check.py
@@ -5,9 +5,12 @@ from lib import *
 
 polling_interval = 60 * 1
 stats_interval = 60 * 30
-verbose = False
-wall_200 = 8000
+verbose = True
+wall_x00 = 8000
 wall_1000 = 8000
+
+# order size 100 or 200
+qty = 100
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--fix", help="add missing and cancel duplicate orders", action="store_true")
@@ -44,12 +47,13 @@ def fix_range():
         # [RANGE] GET TARGET/CURRENT BUY
         # 
 
-        target_buy_first_200 = int(ticker) // 100 * 100 - wall_200
-        if pos and is_long and target_buy_first_200 < liq_price:
-            print("\033[91mTarget First Buy 200 ({:.0f}) < Liq Price ({:.0f}). Use Liq Price\033[00m".format(target_buy_first_200, liq_price)) 
-            target_buy_first_200 = liq_price
-        target_buy_last_1000 = target_buy_first_200 // 1000 * 1000
-        if target_buy_last_1000 >= target_buy_first_200:
+        target_buy_last_x00 = int(ticker) // 100 * 100 - 100
+        target_buy_first_x00 = target_buy_last_x00 - wall_x00
+        if pos and is_long and target_buy_first_x00 < liq_price:
+            print("\033[91mTarget First Buy X00 ({:.0f}) < Liq Price ({:.0f}). Use Liq Price\033[00m".format(target_buy_first_x00, liq_price)) 
+            target_buy_first_x00 = liq_price
+        target_buy_last_1000 = target_buy_first_x00 // 1000 * 1000
+        if target_buy_last_1000 >= target_buy_first_x00:
             target_buy_last_1000 = target_buy_last_1000 - 1000
         target_buy_first_1000 = target_buy_last_1000 - wall_1000
         if pos and is_long and target_buy_first_1000 < liq_price:
@@ -59,18 +63,18 @@ def fix_range():
             print("\033[91mFirst RANGE BUY less than 1000!\033[00m")
             target_buy_first_1000 = 1000
 
-        buy_first_200 = 0
+        buy_first_x00 = 0
         buy_first_1000 = 0
-        buy_last_200 = 0
+        buy_last_x00 = 0
         buy_last_1000 = 0
         buy_orders = get_all_buy_orders(log = False, price_reverse = False)
         for order in buy_orders:
-            if order['price'] // 1000 * 200 == order['orderQty'] or (order['price'] // 1000) * 200 + 200 == order['orderQty']:
-                if buy_last_200 == 0:
-                    buy_first_200 = int(order['price'])
-                    buy_last_200 = buy_first_200
+            if order['price'] // 1000 * qty == order['orderQty'] or (order['price'] // 1000) * qty + qty == order['orderQty']:
+                if buy_last_x00 == 0:
+                    buy_first_x00 = int(order['price'])
+                    buy_last_x00 = buy_first_x00
                 else:
-                    buy_last_200 = int(order['price'])
+                    buy_last_x00 = int(order['price'])
             elif order['price'] == order['orderQty'] and order['orderQty'] % 1000 == 0:
                 if buy_last_1000 == 0:
                     buy_first_1000 = int(order['price'])
@@ -78,39 +82,40 @@ def fix_range():
                 else:
                     buy_last_1000 = int(order['price'])
 
-        if verbose or target_buy_first_1000 != buy_first_1000 or target_buy_last_1000 != buy_last_1000 or target_buy_first_200 != buy_first_200:
+        if verbose or target_buy_first_1000 != buy_first_1000 or target_buy_last_1000 != buy_last_1000 or target_buy_first_x00 != buy_first_x00:
             print("")
-            print("\033[93m{:>20s}{:>20s}{:>20s}{:>20s}\033[00m".format("TARGET  FIRST 1000 BUY", "LAST 1000 BUY", "FIRST 200 BUY", "LAST 200 BUY"))
-            print("\033[93m{:>20.0f}{:>20.0f}{:>20.0f}{:>20s}\033[00m".format(target_buy_first_1000, target_buy_last_1000, target_buy_first_200, "NA"))
-            print("\033[32m{:>20s}{:>20s}{:>20s}{:>20s}\033[00m".format("CURRENT FIRST 1000 BUY", "LAST 1000 BUY", "FIRST 200 BUY", "LAST 200 BUY"))
-            print("\033[32m{:>20.0f}{:>20.0f}{:>20.0f}{:>20.0f}\033[00m".format(buy_first_1000, buy_last_1000, buy_first_200, buy_last_200))
+            print("\033[93m{:>20s}{:>20s}{:>20s}{:>20s}\033[00m".format("TARGET  FIRST 1000 BUY", "LAST 1000 BUY", "FIRST X00 BUY", "LAST X00 BUY"))
+            print("\033[93m{:>20.0f}{:>20.0f}{:>20.0f}{:>20.0f}\033[00m".format(target_buy_first_1000, target_buy_last_1000, target_buy_first_x00, target_buy_last_x00))
+            print("\033[32m{:>20s}{:>20s}{:>20s}{:>20s}\033[00m".format("CURRENT FIRST 1000 BUY", "LAST 1000 BUY", "FIRST X00 BUY", "LAST X00 BUY"))
+            print("\033[32m{:>20.0f}{:>20.0f}{:>20.0f}{:>20.0f}\033[00m".format(buy_first_1000, buy_last_1000, buy_first_x00, buy_last_x00))
             print("")
 
         #
         # [RANGE] GET TARGET/CURRENT SELL
         #
 
-        target_sell_last_200 = int(ticker) // 100 * 100 + wall_200
-        if pos and (not is_long) and liq_price < target_sell_last_200:
-            print("\033[91mTarget Last Sell 200 ({:.0f}) > Liq Price ({:.0f}). Use Liq Price\033[00m".format(target_sell_last_200, liq_price))
-            target_sell_last_200 = liq_price 
-        target_sell_first_1000 = target_sell_last_200 // 1000 * 1000 + 1000
+        target_sell_first_x00 = int(ticker) // 100 * 100 + 100
+        target_sell_last_x00 = target_sell_first_x00 + wall_x00
+        if pos and (not is_long) and liq_price < target_sell_last_x00:
+            print("\033[91mTarget Last Sell X00 ({:.0f}) > Liq Price ({:.0f}). Use Liq Price\033[00m".format(target_sell_last_x00, liq_price))
+            target_sell_last_x00 = liq_price 
+        target_sell_first_1000 = target_sell_last_x00 // 1000 * 1000 + 1000
         target_sell_last_1000 = target_sell_first_1000 + wall_1000
         if pos and (not is_long) and liq_price < target_sell_last_1000:
             print("\033[91mTarget Last Sell 1000 ({:.0f}) > Liq Price ({:.0f}). Use Liq Price\033[00m".format(target_sell_last_1000, liq_price))
             target_sell_last_1000 = liq_price
-        sell_first_200 = 0
+        sell_first_x00 = 0
         sell_first_1000 = 0
-        sell_last_200 = 0
+        sell_last_x00 = 0
         sell_last_1000 = 0
         sell_orders = get_all_sell_orders(log = False, price_reverse = False)
         for order in sell_orders:
-            if order['price'] // 1000 * 200 == order['orderQty'] or (order['price'] // 1000) * 200 - 200 == order['orderQty']:
-                if sell_last_200 == 0:
-                    sell_first_200 = int(order['price'])
-                    sell_last_200 = sell_first_200
+            if order['price'] // 1000 * qty == order['orderQty'] or (order['price'] // 1000) * qty - qty == order['orderQty']:
+                if sell_last_x00 == 0:
+                    sell_first_x00 = int(order['price'])
+                    sell_last_x00 = sell_first_x00
                 else:
-                    sell_last_200 = int(order['price'])
+                    sell_last_x00 = int(order['price'])
             elif order['price'] == order['orderQty'] and order['orderQty'] % 1000 == 0:
                 if sell_last_1000 == 0:
                     sell_first_1000 = int(order['price'])
@@ -118,12 +123,12 @@ def fix_range():
                 else:
                     sell_last_1000 = int(order['price'])
 
-        if verbose or target_sell_last_200 != sell_last_200 or target_sell_first_1000 != sell_first_1000 or target_sell_last_1000 != sell_last_1000:
+        if verbose or target_sell_last_x00 != sell_last_x00 or target_sell_first_1000 != sell_first_1000 or target_sell_last_1000 != sell_last_1000:
             print("")
-            print("\033[93m{:>20s}{:>20s}{:>20s}{:>20s}\033[00m".format("TARGET  FIRST 200 SELL", "LAST 200 SELL", "FIRST 1000 SELL", "LAST 1000 SELL"))
-            print("\033[93m{:>20s}{:>20.0f}{:>20.0f}{:>20.0f}\033[00m".format("NA", target_sell_last_200, target_sell_first_1000, target_sell_last_1000))
-            print("\033[35m{:>20s}{:>20s}{:>20s}{:>20s}\033[00m".format("CURRENT FIRST 200 SELL", "LAST 200 SELL", "FIRST 1000 SELL", "LAST 1000 SELL"))
-            print("\033[35m{:>20.0f}{:>20.0f}{:>20.0f}{:>20.0f}\033[00m".format(sell_first_200, sell_last_200, sell_first_1000, sell_last_1000))
+            print("\033[93m{:>20s}{:>20s}{:>20s}{:>20s}\033[00m".format("TARGET  FIRST X00 SELL", "LAST X00 SELL", "FIRST 1000 SELL", "LAST 1000 SELL"))
+            print("\033[93m{:>20.0f}{:>20.0f}{:>20.0f}{:>20.0f}\033[00m".format(target_sell_first_x00, target_sell_last_x00, target_sell_first_1000, target_sell_last_1000))
+            print("\033[35m{:>20s}{:>20s}{:>20s}{:>20s}\033[00m".format("CURRENT FIRST X00 SELL", "LAST X00 SELL", "FIRST 1000 SELL", "LAST 1000 SELL"))
+            print("\033[35m{:>20.0f}{:>20.0f}{:>20.0f}{:>20.0f}\033[00m".format(sell_first_x00, sell_last_x00, sell_first_1000, sell_last_1000))
             print("")
 
         #
@@ -131,15 +136,15 @@ def fix_range():
         #
 
         # PRICE GOING UP
-        if buy_first_200 < target_buy_first_200 and (not (buy_first_200 == 0 and buy_last_200 == 0)):
+        if buy_first_x00 < target_buy_first_x00 and (not (buy_first_x00 == 0 and buy_last_x00 == 0)):
             if verbose:
-                print("CANCEL BUY 200:{:.0f}..{:.0f}".format(buy_first_200, target_buy_first_200))
-            for p in range(buy_first_200, target_buy_first_200, 100):
+                print("CANCEL BUY X00:{:.0f}..{:.0f}".format(buy_first_x00, target_buy_first_x00))
+            for p in range(buy_first_x00, target_buy_first_x00, 100):
                 if fix_mode:
                     orders = get_orders("Buy", p)
                     time.sleep(3)
                     for order in orders:
-                        if order['price'] // 1000 * 200 == order['orderQty'] or order['price'] // 1000 * 200 == (order['orderQty'] - 200):
+                        if order['price'] // 1000 * qty == order['orderQty'] or order['price'] // 1000 * qty == (order['orderQty'] - qty):
                             cancel_order(order)
                             time.sleep(3)
                 else:
@@ -174,15 +179,15 @@ def fix_range():
                     print(p)
 
         # PRICE GOING DOWN
-        if target_sell_last_200 < sell_last_200 and (not (target_sell_last_200 == 0 and sell_last_200 == 0)):
+        if target_sell_last_x00 < sell_last_x00 and (not (target_sell_last_x00 == 0 and sell_last_x00 == 0)):
             if verbose:
-                print("CANCEL SELL 200:{:.0f}..{:.0f}".format(target_sell_last_200 + 100, sell_last_200 + 100))
-            for p in range(target_sell_last_200 + 100, sell_last_200 + 100, 100):
+                print("CANCEL SELL X00:{:.0f}..{:.0f}".format(target_sell_last_x00 + 100, sell_last_x00 + 100))
+            for p in range(target_sell_last_x00 + 100, sell_last_x00 + 100, 100):
                 if fix_mode:
                     orders = get_orders("Sell", p)
                     time.sleep(3)
                     for order in orders:
-                        if order['price'] // 1000 * 200 == order['orderQty'] or order['price'] // 1000 * 200 == (order['orderQty'] + 200):
+                        if order['price'] // 1000 * qty == order['orderQty'] or order['price'] // 1000 * qty == (order['orderQty'] + qty):
                             cancel_order(order)
                             time.sleep(3)
                 else:
@@ -221,12 +226,12 @@ def fix_range():
         #
 
         # PRICE GOING UP
-        if sell_last_200 < target_sell_last_200:
+        if sell_last_x00 < target_sell_last_x00:
             if verbose:
-                print("ADD SELL 200:{:.0f}..{:.0f}".format(sell_last_200 + 100, target_sell_last_200 + 100))
-            for p in range(sell_last_200 + 100, target_sell_last_200 + 100, 100):
+                print("ADD SELL X00:{:.0f}..{:.0f}".format(sell_last_x00 + 100, target_sell_last_x00 + 100))
+            for p in range(sell_last_x00 + 100, target_sell_last_x00 + 100, 100):
                 if fix_mode:
-                    sell(p, (p // 1000) * 200)
+                    sell(p, (p // 1000) * qty)
                     time.sleep(3)
                 else:
                     print(p)
@@ -254,12 +259,12 @@ def fix_range():
                     print(p)
 
         # PRICE GOING DOWN
-        if target_buy_first_200 < buy_first_200:
+        if target_buy_first_x00 < buy_first_x00:
             if verbose:
-                print("ADD BUY 200:{:.0f}..{:.0f}".format(target_buy_first_200, buy_first_200))
-            for p in range(target_buy_first_200, buy_first_200, 100):
+                print("ADD BUY X00:{:.0f}..{:.0f}".format(target_buy_first_x00, buy_first_x00))
+            for p in range(target_buy_first_x00, buy_first_x00, 100):
                 if fix_mode:
-                    buy(p, (p // 1000) * 200)
+                    buy(p, (p // 1000) * qty)
                     time.sleep(3)
                 else:
                     print(p)
