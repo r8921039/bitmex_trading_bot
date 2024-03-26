3d6dcdc5a283022b31e46bd63db0bfeb050463ef
r8921039 <r8921039@hotmail.com>
2024-03-26 15:56:09 +0000

    add sort to test.py

--- test.py
+++ test.py
@@ -3,8 +3,8 @@
 import argparse
 from lib import *
 
-#side = "Sell"
-side = "Buy"
+side = "Sell"
+#side = "Buy"
 
 now = pytz.utc.localize(datetime.datetime.utcnow())
 
@@ -12,11 +12,13 @@ os.system('clear')
 print("\033[93m{:>15s}{:>30s} (UTC)\033[00m".format("TEST", now.strftime("%d/%m/%Y %H:%M:%S")))
 #show_ticker()
 
-orders = client.Order.Order_getOrders(symbol=symbol, reverse=False, count=200, columns='side,price,orderQty,timestamp,orderID', filter=json.dumps({'open' : True, 'side' : side})).result()[0]
+orders = client.Order.Order_getOrders(symbol=symbol, reverse=False, count=200, columns='side,price,orderQty,timestamp,orderID,ordStatus', filter=json.dumps({'open' : True, 'side' : side})).result()[0]
+orders = sorted(orders, key = lambda i: i['price'], reverse=True)
+
 #out = json.dumps({'open' : 'true', 'side' : side})
 #out = json.dumps({'open' : True, 'side' : side})
 #print(out)
 
 for order in orders:
-    print("{:>15.0f}".format(order['price']))
+    print("{:<15.0f}{:<15s}{:>30s}{:>15s}".format(order['price'], order['side'], order['orderID'], order['ordStatus']))
 
